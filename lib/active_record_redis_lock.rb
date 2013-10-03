beaed91b22af55c99d7b0c620afd292c8369e6d6
Aaron Lifton <aaronlifton@gmail.com>
2013-10-03 14:00:10 -0700

    Update active_record_redis_lock.rb

--- lib/active_record_redis_lock.rb
+++ lib/active_record_redis_lock.rb
@@ -5,7 +5,6 @@ require 'mlanett-redis-lock'
 module ActiveRecordRedisLock
   def redis_lock(user, action_name, &blk)
     Redis.current.lock("#{user.try(:id) || 'anon'}.#{action_name}", life: 1) 
-      binding.pry
       blk.call(self)
       # logger.debug("redis_locked called @ #{Time.now.to_i}") if debug
     end
