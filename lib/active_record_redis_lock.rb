395e898515d6252977a6176d609ce0a4ff21af91
Aaron Lifton <theflubba@gmail.com>
2013-10-03 17:34:52 -0700

    allow custom options

--- lib/active_record_redis_lock.rb
+++ lib/active_record_redis_lock.rb
@@ -1,6 +1,7 @@
 if defined?(Redis) && Redis.try(:current).respond_to?(:lock)
   module ActiveRecordRedisLock
-    def redis_lock(ob, action_name, &blk)
+    def redis_lock(ob, action_name, options = {}, &blk)
+      options = {life: 1}.merge!(options)
       klass = ob.class.name
       caller = if ob.superclass == ActiveRecord::Base 
         ob.try(:id)
@@ -8,7 +9,7 @@ if defined?(Redis) && Redis.try(:current).respond_to?(:lock)
         ob.to_s
       end
 
-      Redis.current.lock("#{klass}.#{caller || 'anon'}.#{action_name}", life: 1) do 
+      Redis.current.lock("#{klass}.#{caller || 'anon'}.#{action_name}", options) do 
         blk.call(self)
         # logger.debug("redis_locked called @ #{Time.now.to_i}") if debug
       end
