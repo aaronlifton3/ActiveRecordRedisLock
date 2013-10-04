3346d563fc24809e0b9ddb839bde15d8821d851f
Aaron Lifton <aaronlifton@gmail.com>
2013-10-03 21:26:17 -0700

    Update active_record_redis_lock.rb

--- lib/active_record_redis_lock.rb
+++ lib/active_record_redis_lock.rb
@@ -3,13 +3,13 @@ if defined?(Redis) && Redis.try(:current).respond_to?(:lock)
     def redis_lock(ob, action_name, options = {}, &blk)
       options = {life: 1}.merge!(options)
       klass = ob.class.name
-      caller = if ob.superclass == ActiveRecord::Base 
+      caller_id = if ob.superclass == ActiveRecord::Base 
         ob.try(:id)
       else
         ob.to_s
       end
 
-      Redis.current.lock("#{klass}.#{caller || 'anon'}.#{action_name}", options) do 
+      Redis.current.lock("#{klass}.#{caller_id || 'anon'}.#{action_name}", options) do 
         blk.call(self)
         # logger.debug("redis_lock called @ #{Time.now.to_i}") if options[:debug]
       end
